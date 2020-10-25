3d56c12ace3ee9fff33099024eb03cc96941fdf9
devjapa <leandrotrovilho@gmail.com>
2020-10-25 01:37:25 -0300

    User Details Git

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -3,7 +3,7 @@ import { makeRequest } from 'core/utils/request';
 import React, { useState } from 'react';
 import './styles.scss';
 
-type UserResponse = {
+type User = {
     empresa: string;
     site: string;
     localidade: string;
@@ -11,6 +11,7 @@ type UserResponse = {
     repositorios: number;
     seguidores: number;
     seguindo: number;
+    imgUrl: string;
 }
 
 type FormState = {
@@ -21,6 +22,8 @@ const Search = () => {
     const [formData, setFormData] = useState<FormState>({
         user: ''
     });
+
+    const [userResponse, setUserResponse] = useState<User>();
 
     const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const name = event.target.name;
@@ -35,31 +38,79 @@ const Search = () => {
         }
 
         console.log(payload);
-        makeRequest({ url: `/users/${payload.user}`})
-        .then((response) => console.log(response.data));
-        
+        makeRequest({ url: `/users/${payload.user}` })
+            .then((response) => console.log(response.data))
+            .catch(error => console.log(error));
+
     }
 
     return (
-        <div className="search-area">
-            <form onSubmit={handleSubmit}>
-                <h1 className="text-title-search">
-                    Encontre um perfil Github
+        <div>
+            <div className="search-area">
+                <form onSubmit={handleSubmit}>
+                    <h1 className="text-title-search">
+                        Encontre um perfil Github
             </h1>
-                <input
-                    value={formData.user}
-                    className="search-box"
-                    name="user"
-                    type="text"
-                    onChange={handleOnChange}
-                    placeholder="Usuário Github"
-                />
+                    <input
+                        value={formData.user}
+                        className="search-box"
+                        name="user"
+                        type="text"
+                        onChange={handleOnChange}
+                        placeholder="Usuário Github"
+                    />
 
-                <div className="search-area-button">
-                    <Button text="Encontrar" />
+                    <div className="search-area-button">
+                        <Button text="Encontrar" />
+                    </div>
+                </form>
+            </div>
+
+            <div className="result-area">
+                <div className="image-container">
+                {/* <img src={product.imgUrl} alt={product.name}/> */}
                 </div>
-            </form>
+
+                <div className="user-details-container ">
+                    <div className="repo-container">
+                        <div className="git-repo">
+                            <text className="repo-text">Repositórios públicos: </text>
+                        </div>
+
+                        <div className="git-seguidores">
+                            <text className="repo-text">Seguidores: </text>
+                        </div>
+
+                        <div className="git-seguindo">
+                            <text className="repo-text">Seguindo: </text>
+                        </div>
+
+                    </div>
+                    <text className="info">Informações</text>
+
+                    <div className="field-empresa">
+                        <text className="text-empresa">Empresa: </text>
+                    </div>
+
+                    <div className="field-empresa">
+                        <text className="text-empresa">Website/Blog: </text>
+                    </div>
+
+                    <div className="field-empresa">
+                        <text className="text-empresa">Localidade: </text>
+                    </div>
+
+                    <div className="field-empresa">
+                        <text className="text-empresa">Membro desde: </text>
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
+
+
     );
 
 }
